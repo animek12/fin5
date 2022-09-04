a2e79cac85d04001c8e89ed29bd7f390fd9257a8
animek12 <111511065+animek12@users.noreply.github.com>
2022-09-04 08:56:12 +0800

    Update biodata.js

--- plugins/biodata.js
+++ plugins/biodata.js
@@ -3,11 +3,11 @@ let fs = require('fs')
 let handler = async (m) => {
 let jarot = `
 •·––––––––––––––––––––––––––·•
-❑ *Nama* : Jarot
-❑ *Umur* : 18 Tahun
-❑ *Kelas* : Lulus
-❑ *Ulang Tahun* : 30 Januari 2004
-❑ *Alamat* : AS, AS, Pelabuhan
+❑ *Nama* : Alfin
+❑ *Umur* : 17 Tahun
+❑ *Kelas* : 11
+❑ *Ulang Tahun* : 20 maret 2005
+❑ *Alamat* : Kaltim
 •·––––––––––––––––––––––––––·•
 	`.trim()
   let message = await prepareWAMessageMedia({ image: await (await require('node-fetch')(fotonya7)).buffer()}, { upload: conn.waUploadToServer }) 
@@ -20,14 +20,14 @@ let jarot = `
            hydratedButtons: [{
              urlButton: {
                displayText: '𝙂𝙧𝙤𝙪𝙥 𝘽𝙤𝙩 𝙊𝙛𝙛𝙘 🐈',
-               url: 'https://chat.whatsapp.com/IqZke0cAG6G3iVsnB9myfL'
+               url: 'https://linktr.ee/fin222'
              }
 
            },
                 {
                urlButton: {
                displayText: '𝙇𝙞𝙣𝙠 𝙔𝙤𝙪𝙏𝙪𝙗𝙚 🌍',
-               url: 'https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ'
+               url: 'https://youtube.com/channel/UCCZFKpk85uGzGdGk9kUZUIA'
              }
 
            },
