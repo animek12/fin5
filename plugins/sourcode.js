4399bb8f1c13165911dea2cbbd79c8599527db9e
animek12 <111511065+animek12@users.noreply.github.com>
2022-09-04 09:23:42 +0800

    Update sourcode.js

--- plugins/sourcode.js
+++ plugins/sourcode.js
@@ -5,15 +5,12 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, 'https://telegra.ph/file/239f046a928370340401f.jpg',
 `Hi Kak @${m.sender.split('@')[0]} 
 
-◪ 📮 *SCRIPT BOT*
-│ *Script :* 
-│ ╰ https://youtu.be/FHntJPk2PkU
-│ *Mampir Youtube Owner Banh:* 
-│ ╰ https://tinyurl.com/2qsejxnw
+◪ 📮 *SOURCE BOT*
+│ *Source code :* 
+│ https://linktr.ee/fin222
 ╰──────────═┅═──────────
 `,`📍 *N o t e :* 
-• Jangan lupa mampir Youtube Owner Kak!
-• Jangan Lupa kasih star & kasih credit
+• Jangan lupa mampir Website Owner Kak!
 
 Official By @${'0'.split('@')[0]}
 Powered By @${`${global.owner[0]}`.split('@')[0]}`, 'Pemilik Bot', '#owner')
